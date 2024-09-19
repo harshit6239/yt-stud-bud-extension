d4867c79d4702c21f80b9682df1286506e48feb6
Harshit Manchanda <128061019+harshit6239@users.noreply.github.com>
2024-09-19 23:37:28 +0530

    fixes

--- background/background.js
+++ background/background.js
@@ -1,42 +1,46 @@
 import axios from "axios";
-axios.defaults.baseURL = 'http://localhost:3000';
+axios.defaults.baseURL = "http://localhost:3000";
 
-chrome.storage.sync.get('token', function(data) {
-    if(data.token){
+chrome.storage.sync.get("token", function (data) {
+    console.log(data);
+    if (data.token) {
         axios.defaults.headers.Authorization = `${data.token}`;
+        console.log("Token set");
     }
 });
 
-chrome.runtime.onMessage.addListener(function(request, sender, sendResponse) {
-    if(request.action == 'login') {
-        axios.post('/api/user/login', request.data, {withCredentials: "true"}).then(response => {
-            chrome.storage.sync.set({token: response.token});
-            chrome.storage.sync.set({token: response.token});
-            axios.defaults.headers.Authorization = `${response.token}`;
-            sendResponse(response.data);
-        }).catch(error => {
-            sendResponse({error: error.response.data.message});
-        });
+chrome.runtime.onMessage.addListener(function (request, sender, sendResponse) {
+    if (request.action == "login") {
+        axios
+            .post("/api/user/login", request.data, {
+                withCredentials: true,
+            })
+            .then((response) => {
+                console.log(response);
+                chrome.storage.sync.set({ token: response.data.token });
+                axios.defaults.headers.Authorization = `${response.data.token}`;
+                sendResponse({ auth: true });
+            })
+            .catch((error) => {
+                sendResponse({ error: error.response.data.message });
+            });
         return true;
-    }
-    if(request.action == 'logout') {
-        axios.post('/api/user/logout', {withCredentials: true}).then(response => {
-            chrome.storage.sync.remove('token');
-            chrome.storage.sync.remove('login');
-            axios.defaults.headers.Authorization = '';
-            sendResponse(response.data);
-        }).catch(error => {
-            sendResponse({error: error.response.data.message});
-        });
+    } else if (request.action == "logout") {
+        chrome.storage.sync.remove("token");
+        axios.defaults.headers.Authorization = "";
+        sendResponse({ auth: false });
         return true;
-    }
-    if(request.action == 'getNotes') {
-        axios.get(`/api/note?videoId=${request.data.vid}`).then(response => {
-            console.log(response.data);
-            sendResponse(response.data);
-        }).catch(error => {
-            sendResponse({error: error.response.data.message});
-        });
+    } else if (request.action == "getNotes") {
+        axios
+            .get(`/api/note?videoId=${request.data.vid}`)
+            .then((response) => {
+                console.log("axios ", response);
+                sendResponse(response.data); // Send the response correctly
+            })
+            .catch((error) => {
+                sendResponse({ error: error.response.data.message });
+            });
+        // Indicate that the response will be sent asynchronously
         return true;
     }
 });
